374331a54b8619eeea0c2f9cc2161be2504c9b4c
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2023-02-20 17:19:26 +0900

    CodeBuild

---
+++ Infrastructure/code_build.tf
@@ -0,0 +1,107 @@
+data "aws_iam_policy_document" "codebuild_assume_role_policy_document" {
+  statement {
+    sid = "CodebuildExecution"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.prefix}-codebuild"
+  assume_role_policy = "${data.aws_iam_policy_document.codebuild_assume_role_policy_document.json}"
+}
+
+data "aws_iam_policy_document" "codebuild_policy_document" {
+  statement {
+    sid = "CloudWatch"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "ECR"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "codebuild_role_policy" {
+  role = "${aws_iam_role.codebuild_role.name}"
+  policy = "${data.aws_iam_policy_document.codebuild_policy_document.json}"
+}
+
+data "template_file" "buildspec_template_file" {
+  template = <<EOF
+version: 0.2
+
+phases:
+  pre_build:
+    commands:
+      - echo pre_build
+      - echo Logging in to Amazon ECR...
+      - $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+
+  build:
+    commands:
+      - echo build
+      - echo build docker image
+      - docker build -t webapp -f ./Server/WebApplication1/Dockerfile ./Server
+
+  post_build:
+    commands:
+      - echo post_build
+      - docker tag webapp:latest ${aws_ecr_repository.webapp.repository_url}:latest
+      - docker push ${aws_ecr_repository.webapp.repository_url}:latest
+EOF
+}
+
+resource "aws_codebuild_project" "codebuild_project" {
+  name          = "${var.prefix}-codebuild-project"
+  description   = "${var.prefix}-codebuild-project"
+  build_timeout = "30"
+  service_role  = "${aws_iam_role.codebuild_role.arn}"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type  = "LOCAL"
+    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:6.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type = "GITHUB"
+    location  = "https://github.com/YoshikazuArimitsu/aspnetcore_ecs.git"
+    buildspec = "${data.template_file.buildspec_template_file.rendered}"
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ASP.NET Core (Docker) & AWS ECS
+
+Dockerでも動くASP.NET Core6 の APIサーバと、Fargateのサンプル。
+
+Fargateは
+
+* プライベートサブネットで動かす
+* ロードバランサ経由でアクセスする
+* ECR から CodePipeline でデプロイする
+    * ECRのイメージが更新されたら自動デプロイする
+* GitHubのコードが更新されたら自動でビルドし、ECRにイメージをPushする
+    * GitHub Actions ではなく CodeBuild を利用する
+
+ここらへん一式揃える、揃えたい。
