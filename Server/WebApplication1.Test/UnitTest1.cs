f0515aad9828175b22a4e18f94e27d3ed355e55f
Y.Arimitsu <yarimit@gmail.com>
2023-02-20 18:07:21 +0900

    テストプロジェクト & テストレポート作成

---
+++ Server/WebApplication1.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace WebApplication1.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+
+        [Fact]
+        public void Test3()
+        {
+
+        }
+    }
+}
